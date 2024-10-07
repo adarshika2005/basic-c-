492840e2645b7e3c1a2a7365385607468dd7784e
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-10-07 22:36:01 +0530

    Add files via upload

---
+++ n.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+  
+  int sum =n*(n+1/2);
+   
+    cout<<"the sum of natural number is "<<sum<<endl;
+    return 0;
+}
