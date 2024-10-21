d32f600e59e0770dc7c32fb012935bfe3d2aef07
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-10-22 00:06:41 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age ;
+    cout<<"enter the age"<<endl;
+    cin>>age;
+    cout<<"the value of age is"<<age<<endl;
+    return 0;
+}
