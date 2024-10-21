1c3c10152ecab47c72daeb9490db80e4d990628c
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-10-22 00:40:34 +0530

    Add files via upload

---
+++ 3var.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int myint=3;
+double mydouble=4.90;
+char mychar='a';
+float myfloat=2.00;
+cout<<"int is "<<myint<<endl;
+cout<<"double is "<<mydouble<<endl;
+cout<<"char is "<<mychar<<endl;
+cout<<"float is "<<myfloat<<endl;
+return 0;
+}
