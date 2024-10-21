5e20d02ab347c970b4e33052bc26e02203d42356
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-10-21 23:55:52 +0530

    Add files via upload

--- var.cpp
+++ var.cpp
@@ -1,9 +1,33 @@
 #include<iostream>
 using namespace std;
+int b=5;//global variable
 int main()
 {
-    int a=6;
-    cout<<"the value of a is:"<<a<<endl;
-    return 0;
+
+    //****value di ho****//
+   /*int a=9;
+    cout<<"the value of a is"<<a<<endl;*/
+ 
+ //******value khud se enter karna ho tab*******//
+
+   // int a;//local variable
+   // cout<<"enter the value of a is"<<endl;
+  //  cin>>a;
+   // cout<<"the value of a is "<<a<<endl;
+
+
+ //******** a and b value are given********//
+   /* int a=9;
+     int b=8;
+    cout<<"the value of a is "<<a<<endl;
+    cout<<"the value of b is"<<b<<endl;*/
+
     
+/******local,global,static variable***********/
+int a=2;
+static int c=8;//static variable
+cout<<a<<endl;
+cout<<b<<endl;
+cout<<c<<endl;
+ return 0;
 }
